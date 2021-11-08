a613cc9b49c2990d4ff06505589eafadeeec8316
Leci1259 <lreid31@gatech.edu>
2021-11-08 14:17:29 -0500

    added distance to model schema

--- models/planModel.js
+++ models/planModel.js
@@ -32,6 +32,9 @@ const planSchema = new Schema({
             },
             sets: {
                 type: Number,
+            },
+            distance: {
+                type: Number,
             }
         }
     ],
